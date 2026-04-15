4e1e6aa8348f3129f612dd427955ebdeadb50b29
Amdadur Rohman <amdadrohman46@student.uns.ac.id>
2026-04-15 17:05:18 +0700

    fix: routing & tsconfig cleanup

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,5 +1,4 @@
 import type { ReactElement } from 'react';
-import { createHashRouter, Navigate } from 'react-router-dom';
 import { createBrowserRouter, Navigate } from 'react-router-dom';
 import { ProtectedRoute } from '../components/common/ProtectedRoute';
 import AppShell from '../components/layout/AppShell';
